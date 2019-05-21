d29dc4b1350fafe2b39684075b4a41456d6e1ef7
Joshua Pickenpaugh <joshuapickenpaugh@hotmail.com>
2019-05-21 10:55:59 -0500

    Last commit of the day.

--- Form1.cs
+++ Form1.cs
@@ -94,18 +94,16 @@ namespace Media_Registration
             string appendedFileContents = appended.ToString();
             Console.WriteLine(appendedFileContents);
 
-            ////Creates writer object, names file and path (need to work on this...):
-            //StreamWriter sW = new StreamWriter("C:\\Users\\JoshPickenpaugh\\Desktop\\TEST.txt");
+            //Creates writer object, names file and path (need to work on this...):
+            //Need to save to same place in file as original.
+            //Need to change name, and add user SELECTED TAPE MOVEMENT & DATE:
+            StreamWriter sW = new StreamWriter("C:\\Users\\JoshPickenpaugh\\Desktop\\TEST.txt");
 
-            ////Cycles through array and writes to txt file:
-            //foreach (string filecontent in appendedFileContents)
-            //{
-            //    sW.WriteLine(filecontent);
-            //}
+            //Writes entire, appended file with line breaks to a new file:
+            sW.Write(appendedFileContents);
 
-            //sW.Close();
+            sW.Close();
 
-            //Test to see if file was created, somehow?
             MessageBox.Show("File Creation Complete!");
 
             //Closes the app:
